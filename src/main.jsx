3ee4619583d83c10669975f546c7ef28343f8975
TPGoFighting <17356397030@163.com>
2026-03-23 18:30:42 +0800

    2.0.3

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/AI-Concentration-Test">
       <App />
 </BrowserRouter>
   </StrictMode>,
